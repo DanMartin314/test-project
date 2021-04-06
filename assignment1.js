c252b4fc70ba93b430691ffcaad0ef35b2f6d59a
Daniel Martin <daniel.lmartin@yahoo.com>
2021-04-05 19:21:25 -0500

    test project

---
+++ assignment1.js
@@ -0,0 +1,13 @@
+//value checker
+
+function valueChecker(argument1, argument2) {
+    if (argument1 === argument2) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+let result = valueChecker(1, "1")
+console.log(result)
---
+++ assignment2.js
@@ -0,0 +1,13 @@
+//sumCalculator 
+
+function sumCalculator(arr) {
+    let sum = 0
+    for (let i = 0; i < arr.length; i++) {
+        sum = sum + arr[i]
+    }
+    return sum
+
+}
+
+let result = sumCalculator([1, 2, 3, 4])
+console.log(result)
